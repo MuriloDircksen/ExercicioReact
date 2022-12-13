6fb7446bc5ac19525be4b6b7f380e9829ebf7b48
Murilod <murilo.dircksen@gmail.com>
2022-12-13 20:28:32 -0300

    exercicio 1

---
+++ exercicio 1/teste/src/About.js
@@ -0,0 +1,14 @@
+function Retorna(){
+    
+    return  <div>
+                <h1>Murilo Dircksen</h1>,
+                <p>Aluno do futuroDev e DevinHouse</p>,
+                <h2>Minhas Habilidades</h2>,
+                <ul>
+                <li>Java</li>
+                <li>JavaScript</li>
+                <li>CSS</li>
+            </ul>
+            </div>
+}
+export default Retorna;
---
+++ exercicio 1/teste/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Retorna from './About';
+
+class App extends React.Component{
+  render(){
+    return<Retorna></Retorna>;
+  }
+}
+export default App;
